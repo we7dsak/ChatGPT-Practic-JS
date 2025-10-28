ad39f801dde7e4874984598c173d6f75ee9d4b99
we7dsak <we7laptop@gmail.com>
2025-10-28 15:52:49 +0200

    done 111

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # ChatGPT-Practic-JS
-Тести, створені chatGPT для практики JS |  Tests created by chatGPT for JS practice | Тесты, созданные chatGPT для практики JS.
+
+Тести, створені chatGPT для практики JS | Tests created by chatGPT for JS practice | Тесты, созданные chatGPT для практики JS.
+
+<!-- Прості задачки -->
+
+<!-- 1
+Напиши функцію sumNumbers(a, b), яка приймає два числа і повертає їхню суму.
+// console.log(sumNumbers(5, 7)); // 12
+ // console.log(sumNumbers(5, 13)); // 18
+ // console.log(sumNumbers(167, 259)); // 426  -->
+
+<!-- 2
+Напиши функцію isEven(number), яка повертає "Парне", якщо число парне, і "Непарне", якщо ні.
+// console.log(isEven(17));// "Непарне"
+// console.log(isEven(144));// "Парне"
+// console.log(isEven(4)); // "Парне"
+// console.log(isEven(7)); // "Непарне" -->
+
+<!-- 3
+Напиши функцію sayHello(name), яка приймає <ім'я> користувача і повертає фразу
+"Привіт, <ім'я>!"
+// console.log(sayHello("Оля")); // "Привіт, Оля!"
+ // console.log(sayHello("Віка"));// "Привіт, Віка!"
+ // console.log(sayHello("Толя")); // "Привіт, Толя!"  -->
+
+<!-- Середні задачки -->
+
+<!-- 1
+Напиши функцію getArraySum(numbers), яка приймає масив чисел і повертає їхню суму.
+console.log(getArraySum([88, 87, 4, 56])); // 235
+console.log(getArraySum([2, 4, 6, 8])); // 20 -->
+
+<!-- 2
+Напиши функцію countVowels(str), яка приймає рядок і повертає кількість голосних букв (a, e, i, o, u, а, е, є, и, і, о, у, я, ю).
+console.log(countVowels("привіт")); // 2 -->
+
+<!-- 3
+Напиши функцію findMax(array), яка приймає масив чисел і повертає найбільше з них (не використовуй Math.max).
+console.log(findMax([3, 9, 1, 5])); // 9 -->
---
+++ js/JAVATEST.js
@@ -0,0 +1,81 @@
+// --------------------ПРОСТІ ЗАДАЧКИ------------------------------
+
+
+// <!-- 1
+// Напиши функцію sumNumbers(a, b), яка приймає два числа і повертає їхню суму.
+
+// function sumNumbers(a, b){
+//     return a + b
+// }
+
+// console.log(sumNumbers(5, 7)); // 12 -->
+// console.log(sumNumbers(5, 13)); // 18 -->
+// console.log(sumNumbers(167, 259)); // 426 -->
+
+// <!-- 2
+// Напиши функцію isEven(number), яка повертає "Парне", якщо число парне, і "Непарне", якщо ні.
+
+// function isEven(number){
+//     if (number % 2 === 0)
+//         return "Парне"
+//     else
+//         return "Непарне"
+// }
+
+// console.log(isEven(17));// "Непарне"
+// console.log(isEven(144));// "Парне"
+// console.log(isEven(4)); // "Парне"
+// console.log(isEven(7)); // "Непарне" -->
+
+
+// <!-- 3
+// Напиши функцію sayHello(name), яка приймає <ім'я> користувача і повертає фразу
+// "Привіт, <ім'я>!"
+
+// function sayHello(name) {
+//     return `Привіт, ${name}`
+// }
+
+// console.log(sayHello("Оля")); // "Привіт, Оля!" -->
+// console.log(sayHello("Віка"));// "Привіт, Віка!" -->
+// console.log(sayHello("Толя")); // "Привіт, Толя!" -->
+
+
+// --------------------СЕРЕДНІ ЗАДАЧКИ------------------------------
+
+// <!-- 1
+// Напиши функцію getArraySum(numbers), яка приймає масив чисел і повертає їхню суму.
+
+// function getArraySum(numbers) {
+//     let total = 0;
+  
+//     for (let i = 0; i < numbers.length; i++) {
+//          total += numbers[i];
+//    }
+    
+//    return total
+        
+    
+
+// }
+
+// console.log(getArraySum([2, 4, 6, 8])); // 20
+
+// console.log(getArraySum([88, 87, 4, 56])); // 235 -->
+
+
+// <!-- 2
+// Напиши функцію countVowels(str), яка приймає рядок
+//  і повертає кількість голосних букв (a, e, i, o, u, а, е, є, и, і, о, у, я, ю).
+
+function countVowels(str) {
+    let total = 0;
+    const wowo = "аеєиіїоуюяaeiou"
+
+    for (let i = 0; i < i.length; i++) {
+        const element = array[i];
+        
+    }
+}
+
+// console.log(countVowels("привіт")); // 2 -->
